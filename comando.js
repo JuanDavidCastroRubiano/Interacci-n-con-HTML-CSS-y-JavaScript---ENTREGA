b371b63ef64c30a30d41c554e59e5a40e23d57f6
juandavidcastrorubiano-svg <juandacastro1985@gmail.com>
2026-03-24 12:20:00 -0500

    ejercicio HTML

---
+++ comando.js
@@ -0,0 +1,23 @@
+let contador = 0;
+
+
+// Funcion para sumar
+
+function sumar() {
+
+    contador = contador + 1;
+
+    document.getElementById("numero").textContent = contador;
+
+}
+
+
+// Funcion para restar
+
+function restar() {
+
+    contador = contador - 1;
+
+    document.getElementById("numero").textContent = contador;
+
+}
