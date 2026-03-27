dd7679a588fbdd233736760f95d2ab1600761211
juandavidcastrorubiano-svg <juandacastro1985@gmail.com>
2026-03-27 09:20:34 -0500

    entrega Final del trabajo

--- comando.js
+++ comando.js
@@ -1,19 +1,15 @@
 let contador = 0;
 
-
 function sumar() {
 
     contador = contador + 1;
-
     document.getElementById("numero").textContent = contador;
 
 }
 
-
 function restar() {
 
     contador = contador - 1;
-
     document.getElementById("numero").textContent = contador;
 
 }
