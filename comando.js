5e4f8274d610bd9d225e7e8111a57d54e64ae2e5
juandavidcastrorubiano-svg <juandacastro1985@gmail.com>
2026-03-30 06:51:59 -0500

    Entrega final  Taller exportación de funciones

--- comando.js
+++ comando.js
@@ -1,15 +1,9 @@
 let contador = 0;
-
 function sumar() {
-
     contador = contador + 1;
     document.getElementById("numero").textContent = contador;
-
 }
-
 function restar() {
-
     contador = contador - 1;
     document.getElementById("numero").textContent = contador;
-
 }
